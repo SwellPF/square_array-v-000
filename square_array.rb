41efb1f71aace124eba4dcba4a7194c079e4a5c0
Paul Fell <swellpf@gmail.com>
2018-09-20 18:46:05 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,8 @@
-require 'pry'
 def square_array(array)
   # your code here
-  # squared_numbers = []
-  # array.each do |number|
-  #      squared_numbers.push(number**2)
-  # end
-  # squared_numbers
-binding.pry  
-  array.collect! {|x| x**2}
-  
-binding.pry
-  
-  array
+   squared_numbers = []
+   array.each do |number|
+        squared_numbers.push(number**2)
+   end
+   squared_numbers
 end
